ee3e70792ba1660af22a6a47bbdf542cdd7a592d
saiteja cherviralla <saiteja.ch.st1@gmail.com>
2023-08-08 11:10:53 +0000

    deleted

--- app/seeduniqueorder/dist/Component.js
+++
@@ -1,1 +0,0 @@
-sap.ui.define(["sap/ui/core/UIComponent","sap/ui/Device","seeduniqueorder/model/models"],function(e,i,t){"use strict";return e.extend("seeduniqueorder.Component",{metadata:{manifest:"json"},init:function(){e.prototype.init.apply(this,arguments);var i=this.getRouter();if(i){i.initialize()}this.setModel(t.createDeviceModel(),"device")}})});
