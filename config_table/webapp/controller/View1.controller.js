22ea759e82c32b67c6c9f5d9966cb4635b7199de
saiteja cherviralla <saiteja.ch.st1@gmail.com>
2023-11-21 13:44:03 +0000

    new changes added

--- config_table/webapp/controller/View1.controller.js
+++ config_table/webapp/controller/View1.controller.js
@@ -6,11 +6,31 @@ sap.ui.define([
      */
     function (Controller) {
         "use strict";
-        var that;
-        return Controller.extend("vcconfigurable.controller.View1", {
+            var that;
+        return Controller.extend("configtable.controller.View1", {
             onInit: function () {
 
                 that = this;
+
+                var oData = that.getOwnerComponent().getModel()
+
+                oData.read("/INTERFACE_TABLE",{
+                    success:function(res)
+                    {
+                        var oModel1 = new sap.ui.model.json.JSONModel()
+                        console.log(res.results)
+
+                        oModel1.setData({
+                            items:res.results
+                        })
+
+                        that.byId("_IDGenList1").setModel(oModel1)
+                    },
+                    error:function(err)
+                    {
+                        console.log(err)
+                    }
+                })
                 
                 var treeModel=[
                     {
@@ -31,8 +51,7 @@ sap.ui.define([
 
                 ]
                 var oModel=new sap.ui.model.json.JSONModel(treeModel);
-                this.getView().setModel(oModel);
+                that.byId("Tree").setModel(oModel);
             }
-
         });
     });
